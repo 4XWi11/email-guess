8f75f1a75056de48ddbcad01342e7be77f7f6a07
4XWi11 <67883454+4XWi11@users.noreply.github.com>
2021-09-24 17:50:32 +0800

    Add files via upload

---
+++ src/hello_world/Welcome.java
@@ -0,0 +1,28 @@
+package hello_world;
+
+import java.util.Scanner;
+
+public class Welcome {
+	
+	
+	public static void main(String[] args) {	//函数
+		final int a;
+		a = 100;
+		int hours=40;
+		int payRate=25;
+		int grossPay=hours*payRate;
+		System.out.print(a);					//不带换行的
+		System.out.println(a);
+		
+		String cityName = "杭州";
+		String cityWel = new String("杭州欢迎你");//
+		//cityName.
+		char i;
+		Scanner scan = new Scanner(System.in);
+//		if(scan.hasNextInt()) {
+//			i=scan.nextInt();
+//		System.out.println(i);
+//		}
+				
+	}
+}
